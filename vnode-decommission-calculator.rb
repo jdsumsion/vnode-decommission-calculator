da219f5060f5d91e082ad72a518c236078ee9ddd
John Sumsion <sumsionjg@familysearch.org>
2018-12-07 17:17:28 -0700

    Rewrite with stddev and last-3-node token counting

--- vnode-decommission-calculator.rb
+++ vnode-decommission-calculator.rb
@@ -9,6 +9,14 @@ unless ARGV.size >= 1 and File.exist? ARGV[0]
   usage_and_exit
 end
 
+###
+# For extra debug output, set DEBUG=true
+DEBUG=false
+
+def debug(*args)
+  puts *args if DEBUG
+end
+
 nodes_to_remove_from_each_rack = 2
 if ARGV.size > 1
   unless ARGV[1] =~ /^\d+$/
@@ -99,52 +107,64 @@ def force_64_bit_overflow(n)
   ((n + 2**63) % 2**64) - 2**63
 end
 
+def count_tokens_in_token_sets(token_sets)
+  token_sets.each_cons(2).reduce(0) do |count, ((start_token, node), (end_token, _))|
+    count + force_64_bit_overflow(end_token - start_token)
+  end
+end
+
 def count_tokens(node, nodes_by_tokens)
-  [ *nodes_by_tokens, nodes_by_tokens.first ].each_cons(2).reduce(0) do |count, ((start_token, node_to_check), (end_token, _))|
-    count + (node.equal?(node_to_check) && force_64_bit_overflow(end_token - start_token) || 0)
+  [ *nodes_by_tokens, *nodes_by_tokens[-3..-1] ].each_cons(4).reduce(0) do |count, token_sets|
+    node_to_check = token_sets[-1][1]
+    count + (node.equal?(node_to_check) && count_tokens_in_token_sets(token_sets) || 0)
   end
 end
 
-def calculate_token_mean_variance_without_node(nodes, ip)
-  nodes.delete_if{|node| ip == node.ip}
-  nodes_by_tokens = Hash[nodes.map{|node| node.tokens.zip([node].cycle)}.flatten(1).sort_by{|token,node| token}]
-  token_counts = nodes.map{|node| count_tokens(node, nodes_by_tokens) }
-
-  mean = token_counts.reduce(:+) / token_counts.size.to_f
-  mean_variance = 0.0
-  token_counts.each do |token_count|
-    mean_variance += (token_count - mean).abs
+def calculate_token_stddev_without_node(nodes_in_rack, ip)
+  nodes_in_rack.delete_if{|node| ip == node.ip}
+  nodes_by_tokens = Hash[nodes_in_rack.map{|node| node.tokens.zip([node].cycle)}.flatten(1).sort_by{|token,node| token}]
+  debug "after removing #{ip}"
+  token_counts = nodes_in_rack.map do |node|
+    token_count = count_tokens(node, nodes_by_tokens)
+    debug "node #{node.ip} has total tokens: #{token_count}"
+    token_count
   end
-  #puts "mean_variance without #{ip} is #{mean_variance}"
-  mean_variance
+
+  sum = token_counts.sum
+  mean = token_counts.sum / token_counts.size.to_f
+  debug "mean: #{mean}"
+  total_squares_from_mean = token_counts.reduce(0) do |total, token_count|
+    total + (token_count - mean) ** 2
+  end
+  variance = total_squares_from_mean / token_counts.size.to_f
+  stddev = Math.sqrt(variance)
+  debug "stddev without #{ip} is #{stddev}"
+  stddev
 end
 
-def calculate_best_token_distribution(all_nodes, node_permutations)
-  variance_sums_per_permutation = []
+def calculate_best_token_distribution(nodes_in_rack, node_permutations)
+  stddevs_per_permutation = []
   permutation_size = node_permutations[0].size
   node_permutations.each_with_index do |permutation, i|
-    nodes = Array[*all_nodes]
-    mean_variance_sum = 0
-    permutation.each do |node|
-      mean_variance_sum += calculate_token_mean_variance_without_node(nodes, node.ip)
-    end
-    variance_sums_per_permutation[i] = mean_variance_sum
-    #puts "permutation: #{permutation.map(&:ip).join ','}: #{variance_sums_per_permutation[i]}"
+    nodes = Array[*nodes_in_rack]
+    debug "\ntrying permutation: #{permutation.map(&:ip).inspect}"
+    stddevs = permutation.map{|node| calculate_token_stddev_without_node(nodes, node.ip)}
+    stddevs_per_permutation[i] = stddevs.sum / stddevs.size.to_f
   end
 
-  least_variance_index = variance_sums_per_permutation.index(variance_sums_per_permutation.min)
-  least_variance = variance_sums_per_permutation[least_variance_index]
-  first_preferred_ips = node_permutations[least_variance_index].map(&:ip)
+  least_stddev_index = stddevs_per_permutation.index(stddevs_per_permutation.min)
+  least_stddev = stddevs_per_permutation[least_stddev_index]
+  first_preferred_ips = node_permutations[least_stddev_index].map(&:ip)
 
-  next_preferred_variance = nil
-  variance_sums_per_permutation.each_with_index do |sum, i|
+  next_preferred_stddev = nil
+  stddevs_per_permutation.each_with_index do |sum, i|
     next if node_permutations[i].find{|node| first_preferred_ips.include? node.ip}
-    next_preferred_variance = [ next_preferred_variance || sum+1, sum ].min
+    next_preferred_stddev = [ next_preferred_stddev || sum+1, sum ].min
   end
-  next_least_variance_index = variance_sums_per_permutation.index(next_preferred_variance)
-  next_least_variance = variance_sums_per_permutation[next_least_variance_index]
-  next_preferred_ips = node_permutations[next_least_variance_index].map(&:ip)
-  [ first_preferred_ips, next_preferred_ips, least_variance, next_least_variance ]
+  next_least_stddev_index = stddevs_per_permutation.index(next_preferred_stddev)
+  next_least_stddev = stddevs_per_permutation[next_least_stddev_index]
+  next_preferred_ips = node_permutations[next_least_stddev_index].map(&:ip)
+  [ first_preferred_ips, next_preferred_ips, least_stddev, next_least_stddev ]
 end
 
 def format_ip_list(ips)
@@ -162,15 +182,16 @@ nodes_by_rack.each do |rack, nodes|
     node_permutations = nodes.permutation(nodes_to_remove_from_each_rack).to_a
     best_nodes_to_decommission,
     next_best_nodes_to_decommission,
-    least_variance,
-    next_least_variance = calculate_best_token_distribution(nodes, node_permutations)
+    least_stddev,
+    next_least_stddev = calculate_best_token_distribution(nodes, node_permutations)
     puts "=> %s: %s [%.2e] (next best: %s [%.2e])" % [
       rack,
       format_ip_list(best_nodes_to_decommission),
-      least_variance,
+      least_stddev,
       format_ip_list(next_best_nodes_to_decommission),
-      next_least_variance
+      next_least_stddev
     ]
+    debug "\n*****************************************************************************"
   else
     puts "=> #{rack}: unable to remove #{nodes_to_remove_from_each_rack} nodes from this rack"
   end
